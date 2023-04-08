3e6413a078ae70ab11c205cb9bcb56ab4c73c866
ahalamora1981 <96462464+ahalamora1981@users.noreply.github.com>
2023-04-08 23:08:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import time
 # Define function to generate bot response
 def generate_response(user_input, history):
     if history:
-        system_msg = history[-1][1].strip("Bot: ")
+        system_msg = history[-1][1].strip("订票机器人: ")
     else:
         system_msg = '''### 从用户输入中抽取的发出城市、到达城市、航班日期、航空公司、仓位等级，并回复。如有缺失的信息，请向对方发问；如信息完整，就不要发问。 ###
 
@@ -24,21 +24,16 @@ def generate_response(user_input, history):
 """
 
 以下是您的航班信息：'''
-        
-    st.write("System Msg: ", system_msg)
-    
-#     openai.ChatCompletion.create(
-#         model="gpt-3.5-turbo",
-#         messages=[
-#             {"role": "system", "content": "You are a helpful assistant."},
-#             {"role": "user", "content": "Who won the world series in 2020?"},
-#             {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-#             {"role": "user", "content": "Where was it played?"}
-#         ],
-#         temperature = 0
-#     )
-    
-    bot_response = "You are great!" + str(time.time())
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_msg},
+            {"role": "user", "content": user_input}
+        ],
+        temperature = 0
+    )
+    bot_response = response['choices'][0]['message']['content']
     return bot_response
 
 # Initialize conversation history
@@ -53,14 +48,14 @@ st.subheader("对话内容：")
 conversation_container = st.container()
 
 # Add text input field
-user_input = st.text_input("You: ")
+user_input = st.text_input("请输入: ")
 
 # Add button to submit user input
 if st.button("Submit", use_container_width=True):
     # Generate bot response
     bot_response = generate_response(user_input, st.session_state.conversation_history)
     # Add user input and bot response to conversation history
-    st.session_state.conversation_history.append(("You: " + user_input, "Bot: " + bot_response))
+    st.session_state.conversation_history.append(("用户: " + user_input, "订票机器人: " + bot_response))
     # Display conversation history
     for user, bot in st.session_state.conversation_history:
         conversation_container.text(user)
