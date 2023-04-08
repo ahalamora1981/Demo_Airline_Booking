7eae10b19309de3381515a857d6ed8b9d56426ff
ahalamora1981 <96462464+ahalamora1981@users.noreply.github.com>
2023-04-08 22:35:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,24 +10,24 @@ def generate_response(user_input):
     return bot_response
 
 # Initialize conversation history
-if "conversation_history" not in locals() or "conversation_history" not in globals():
-    conversation_history = []
+if not st.session_state.conversation_history:
+    st.session_state.conversation_history = []
 
 # Add text input field
 user_input = st.text_input("You: ")
 
 # Add button to submit user input
 if st.button("Submit"):
-    st.write(conversation_history)
+    st.write(st.session_state.conversation_history)
     # Generate bot response
     bot_response = generate_response(user_input)
     # Add user input and bot response to conversation history
-    conversation_history.append(("You: " + user_input, "Bot: " + bot_response))
+    st.session_state.conversation_history.append(("You: " + user_input, "Bot: " + bot_response))
     # Display conversation history
-    for user, bot in conversation_history:
+    for user, bot in st.session_state.conversation_history:
         st.text(user)
         st.text(bot)
 
 # Add button to clear conversation history
 if st.button("Clear history"):
-    conversation_history.clear()
+    st.session_state.conversation_history.clear()
