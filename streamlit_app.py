7c238cf9e1289bc6c9333b79aa6268947416e7f1
ahalamora1981 <96462464+ahalamora1981@users.noreply.github.com>
2023-04-08 22:14:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,20 @@
 import streamlit as st
 
 # Set page title
-st.title("Input-Output App")
+st.title("Chat with Bot")
+
+# Define function to generate bot response
+def generate_response(user_input):
+    # Do some processing here to generate bot response
+    bot_response = "I'm sorry, I don't understand. Please try again."
+    return bot_response
 
 # Add text input field
-user_input = st.text_input("Enter some text:")
+user_input = st.text_input("You: ")
 
-# Add text output field
-st.write("Output:", user_input)
+# Add button to submit user input
+if st.button("Submit"):
+    # Generate bot response
+    bot_response = generate_response(user_input)
+    # Display bot response
+    st.text("Bot: " + bot_response)
