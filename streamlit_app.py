2b27d5ed1929334e24cb24834bf383c25e575267
ahalamora1981 <96462464+ahalamora1981@users.noreply.github.com>
2023-04-09 08:52:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,6 +63,8 @@ with st.form(key='my_form', clear_on_submit=True):
     user_input = st.text_input("请输入: ")
     submit = st.form_submit_button(label='提交', use_container_width=True)
     clear_history = st.form_submit_button("清空历史对话记录", use_container_width=True)
+    if not st.session_state.conversation_history:
+        st.write("对话历史记录已清空")
 
 # Add button to submit user input
 # if st.button("Submit", use_container_width=True):
